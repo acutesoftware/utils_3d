6c06632bc46402a72fcb6d8700c2ef80a2fdfe5e
Duncan Murray <djmurray@gmail.com>
2019-12-28 00:34:54 +1030

    setup base script structure

--- UE4_git.py
+++ UE4_git.py
@@ -1,5 +1,5 @@
-#
-
+#!/usr/bin/python3
+# coding: utf-8
 # UE4_git.py
 #
 # script to setup git for Unreal Engine
@@ -8,3 +8,19 @@
 # - generates list of all files, adds list to repo 
 # - setup README.md
 # - adds all UE4 assets less than 50M
+
+pth = 'D:\\docs\\Unreal Projects\\sanct'
+
+def TEST():
+    print('setting up git script for UE4 package for ' + pth)
+    lst = get_list_of_UE4_files(pth)
+
+def get_list_of_UE4_files(pth):
+    res = []
+
+
+    return res 
+
+
+if __name__ == '__main__':  
+    TEST()        
