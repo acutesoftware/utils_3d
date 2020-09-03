c2653de250cdb5a2e5c1c6225c022b08fdd2fb0b
Duncan Murray <djmurray@gmail.com>
2020-09-03 22:25:37 +0930

    mapping drives on Windows 10 on startup

---
+++ map_drives.bat
@@ -0,0 +1,7 @@
+net use * /del /y
+net use m: \\fangorn\music
+net use n: \\fangorn\user
+net use p: \\fangorn\photo
+net use s: \\fangorn\incoming
+net use u: \\fangorn\user\duncan\C\user
+net use w: \\fangorn\Marita
